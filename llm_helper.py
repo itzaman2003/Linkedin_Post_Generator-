1b94893dc5f7540850c6fe462f32999942819a3f
itzaman2003 <ingleaman22@gmail.com>
2025-07-10 22:32:44 +0530

    Your message here

--- llm_helper.py
+++ llm_helper.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.2-90b-vision-preview")
+llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="compound-beta-mini")
 
 
 if __name__ == "__main__":
